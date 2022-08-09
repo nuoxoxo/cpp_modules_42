4d69bb5fb54971634afc604da9dc9900202cd7db
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-09 23:41:12 +0200

    del. color.hpp . subject dictates

--- oo1/oo_brainz/Zombie.hpp
+++ oo1/oo_brainz/Zombie.hpp
@@ -15,7 +15,16 @@
 
 # include "iostream"
 # include "string"
-# include "Color.hpp"
+//# include "Color.hpp"
+
+# define _yellow	"\x1b[33m"
+# define _red		"\x1b[31m"
+# define _green		"\x1b[32m"
+# define _blue		"\x1b[34m"
+# define _mag		"\x1b[35m"
+# define _cyan		"\x1b[36m"
+# define _ital		"\x1b[3m"
+# define _reset		"\x1b[0m"
 
 class	Zombie
 {
