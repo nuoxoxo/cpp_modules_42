2e1a7afcbfd84b20cd3244e191ee7746d64e3bbb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-10 19:14:55 +0200

    init oo6 karen filter

---
+++ oo1/oo6_karen_ft/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ...      :::::::    */
+/*   _                                                  ...      :::    :::   */
+/*                                                    ... ...         :::     */
+/*   By: nuo <marvin@42.fr>                         ...  ...       :::        */
+/*                                                ...........   :::           */
+/*   Created: ____/__/__ __:__:__ by nuo               ...    :::             */
+/*   Updated: ____/__/__ __:__:__ by nuo              ...   ::::::::.fi       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+int	main( void )
+{
+	Karen		kafka;
+
+	kafka.complain("debug");
+	kafka.complain("info");
+	kafka.complain("warning");
+	kafka.complain("error");
+	kafka.complain("john conway");
+}
