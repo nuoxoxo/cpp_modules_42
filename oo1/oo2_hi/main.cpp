1be2b59597c6c39af56fe58a939a87e6a0ca6507
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-10 02:51:23 +0200

    fi. oo1 . oo2 . at 0251am

---
+++ oo1/oo2_hi/main.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ...      :::::::    */
+/*   _                                                  ...      :::    :::   */
+/*                                                    ... ...         :::     */
+/*   By: nxu <marvin@42.fr>                         ...  ...       :::        */
+/*                                                ...........   :::           */
+/*   Created: ____/__/__ __:__:__ by nxu               ...    :::             */
+/*   Updated: ____/__/__ __:__:__ by nxu              ...   ::::::::.fi       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iostream"
+#include "string"
+# define _y "\x1b[33m"
+# define _r "\x1b[0m"
+# define _c "\x1b[36m"
+
+int	main(const int c, const char **v)
+{
+	std::string		s;
+	std::string		*stringPTR = & s;
+	std::string		&stringREF = s;
+
+	s = "HI THIS IS BRAIN";
+	std::cout << _y "\nthe address of \"HI THIS IS BRAIN\"   : " _r << \
+		& s << '\n';
+
+	// ------------8<------------
+
+	std::cout << _y "the address pointed to by stringPTR : " _r << \
+		& (*stringPTR) << '\n';
+
+	// ------------8<------------
+
+	std::cout << _y "the address pointed to by stringREF : " _r << \
+		& stringREF << "\n\n";
+
+	// ------------8<------------
+
+	std::cout << _c "the brain : " _r << s << '\n';
+	std::cout << _c "stringPTR : " _r << *stringPTR << '\n';
+	std::cout << _c "stringREF : " _r << stringREF << "\n\n";
+	(void) c;
+	(void) v;
+	return (0);
+}
