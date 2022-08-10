1ccfbf2a5d95de8c4a407f7c83128d9f45c6b50e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-10 18:34:02 +0200

    + oo5 . all msg defined in .h

--- oo1/oo5_karen/Karen.cpp
+++ oo1/oo5_karen/Karen.cpp
@@ -15,7 +15,39 @@
 Karen::Karen(void) {}
 Karen::~Karen(void) {}
 
+void	Karen::debug(void)	{ std::cout << DEBUG_LEVEL; }
+void	Karen::info(void)	{ std::cout << INFO_LEVEL; }
+void	Karen::warning(void)	{ std::cout << WARNING_LEVEL; }
+void	Karen::error(void)	{ std::cout << ERROR_LEVEL; }
+
 void	Karen::complain(std::string level)
 {
 
 }
+
+/*
+
+• "DEBUG" level: 
+
+	"I love to get extra bacon 
+	for my 7XL-double-cheese-triple-pickle-special-ketchup burger. 
+	I just love it!"
+
+• "INFO" level: 
+
+	"I cannot believe adding extra bacon cost more money. 
+	You don’t put enough! 
+	If you did I would not have to ask for it!"
+
+• "WARNING" level: 
+
+	"I think I deserve to have some extra bacon for free. 
+	I’ve been coming here for years 
+	and you just started working here last month."
+
+• "ERROR" level: 
+
+	"This is unacceptable, 
+	I want to speak to the manager now."
+
+*/
--- oo1/oo5_karen/Karen.hpp
+++ oo1/oo5_karen/Karen.hpp
@@ -33,4 +33,39 @@ class	Karen
 		void	complain(std::string);
 };
 
+# define DEBUG_LEVEL "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it! \n"
+
+# define INFO_LEVEL "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it! \n"
+
+# define WARNING_LEVEL "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month. \n"
+
+# define ERROR_LEVEL "This is unacceptable, I want to speak to the manager now. \n"
+
 #endif
+
+/*
+
+• "DEBUG" level: 
+
+	"I love to get extra bacon 
+	for my 7XL-double-cheese-triple-pickle-special-ketchup burger. 
+	I just love it!"
+
+• "INFO" level: 
+
+	"I cannot believe adding extra bacon cost more money. 
+	You don’t put enough! 
+	If you did I would not have to ask for it!"
+
+• "WARNING" level: 
+
+	"I think I deserve to have some extra bacon for free. 
+	I’ve been coming here for years 
+	and you just started working here last month."
+
+• "ERROR" level: 
+
+	"This is unacceptable, 
+	I want to speak to the manager now."
+
+*/
