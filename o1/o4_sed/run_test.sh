9bf13f00b0c65d29025c26e01a8f411c3d7ff93d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-13 20:19:15 +0200

    minor mod.

--- o1/o4_sed/run_test.sh
+++ o1/o4_sed/run_test.sh
@@ -12,14 +12,14 @@ echo "Test 0. replace ${c1}\"aa\"${rs} with ${c2}\"XX\"${rs} in ${c3}random_finn
 ./replace  testsuite/RANDOM_FINNISH.replace  XX  aa
 
 diff -s  testsuite/random_finnish  testsuite/RANDOM_FINNISH.REPLACE.replace
-echo
+#echo
 
 echo "test 1. replace ${c1}\"e\"${rs} with ${c2}\"(o ^ . ^)o\"${rs} in ${c3}random_portugese ${rs}"
 ./replace  testsuite/random_portugese  e  "(o ^ . ^)o"
 ./replace  testsuite/RANDOM_PORTUGESE.replace  "(o ^ . ^)o" e
 
 diff -s  testsuite/random_portugese  testsuite/RANDOM_PORTUGESE.REPLACE.replace
-echo
+#echo
 
 echo "test 2. replace ${c1}\"do\"${rs} with ${c2}\"QWERTY\"${rs} in ${c3}random_dutch ${rs}"
 
@@ -27,7 +27,7 @@ echo "test 2. replace ${c1}\"do\"${rs} with ${c2}\"QWERTY\"${rs} in ${c3}random_
 ./replace  testsuite/RANDOM_DUTCH.replace  QWERTY do
 
 diff -s  testsuite/random_dutch  testsuite/RANDOM_DUTCH.REPLACE.replace
-echo
+#echo
 
 echo "test 3. replace ${c1}\"se\"${rs} with ${c2}\"Distant Star\"${rs} in ${c3}random_italian ${rs}"
 
@@ -35,7 +35,7 @@ echo "test 3. replace ${c1}\"se\"${rs} with ${c2}\"Distant Star\"${rs} in ${c3}r
 ./replace  testsuite/RANDOM_ITALIAN.replace  "Distant Stars" se
 
 diff -s  testsuite/random_italian  testsuite/RANDOM_ITALIAN.REPLACE.replace
-echo
+#echo
 
 
 echo "test 4. replace ${c1}\"te\"${rs} with ${c2}\"4242\"${rs} in ${c3}random_german ${rs}"
