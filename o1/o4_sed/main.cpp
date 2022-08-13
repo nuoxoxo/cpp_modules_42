27fadf9af73ba263e998cdaecf1bcd17f11afdc9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-13 16:20:46 +0200

    bugs in repalce tofix

--- o1/o4_sed/main.cpp
+++ o1/o4_sed/main.cpp
@@ -10,6 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Sed.hpp"
+#include "Stream_editor.hpp"
 
+void	usage(void);
 
+int	main(int c, char *v[])
+{
+	Stream_editor		sed;
+
+	if (c ^ 4)
+		return (usage(), 1);
+	if (!sed.setup_infile(v[1]))
+	{
+		std::cout << "err 1 \n";
+	}
+	if (!sed.replace(v[2], v[3]))
+	{
+		std::cout << "err 2 \n";
+	}
+
+}
+
+void	usage(void)
+{
+	std::cout << "usage \n";
+}
