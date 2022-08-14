e110c664ca1f87d731d58c86ededc1cc321dcf51
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-14 19:41:21 +0200

    + virtual for filter postbox

--- postbox/_filter/main.cpp
+++ postbox/_filter/main.cpp
@@ -16,15 +16,15 @@
 
 class	Karen {
 	private:
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
+		void		debug(void);
+		void		info(void);
+		void		warning(void);
+		void		error(void);
 	public:
-
 		Karen(void);
-		~Karen(void);
-		void	complain(std::string);
+		//~Karen(void);
+		virtual		~Karen(void);
+		void		complain(std::string);
 };
 
 //	prototyping
