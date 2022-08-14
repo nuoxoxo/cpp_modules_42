e9df2880d3f327a5bc4e43eb0e88c932bea1c9f6
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-14 19:34:39 +0200

    + virtual for karen destr

--- o1/exo5_karen/Karen.hpp
+++ o1/exo5_karen/Karen.hpp
@@ -19,24 +19,25 @@
 class	Karen
 {
 	private:
-
-		void	debug(void);
-		void	info(void);
-		void	warning(void);
-		void	error(void);
-
+		void		debug(void);
+		void		info(void);
+		void		warning(void);
+		void		error(void);
 	public:
-
 		Karen(void);
-		~Karen(void);
-
-		void	complain(std::string);
+		virtual		~Karen(void);
+		void		complain(std::string);
 };
+
+//	def all complaints
 
 # define DEBUG_LEVEL "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it! \n\n"
 # define INFO_LEVEL "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it! \n\n"
 # define WARNING_LEVEL "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month. \n\n"
 # define ERROR_LEVEL "This is unacceptable, I want to speak to the manager now. \n\n"
+
+//	def. colors
+
 # define _y "\x1b[33m"
 # define _g "\x1b[32;1m"
 # define _i "\x1b[3m"
