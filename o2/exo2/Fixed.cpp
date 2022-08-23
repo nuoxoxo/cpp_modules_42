51f7eb52cdba7e9e257f1419893364b2b3dca8cb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-08-24 00:38:41 +0200

    edit ex02 . del increment inter-dependency

--- o2/exo2/Fixed.cpp
+++ o2/exo2/Fixed.cpp
@@ -123,10 +123,10 @@ Fixed	& Fixed::operator ++ (void)
 
 Fixed	Fixed::operator ++ (int)
 {
-	Fixed	old(*this);
+	Fixed	original(*this);
 
-	++(*this);
-	return (old);
+	(*this).fixed_point_value += 1;
+	return (original);
 }
 
 Fixed	& Fixed::operator -- (void)
@@ -137,10 +137,10 @@ Fixed	& Fixed::operator -- (void)
 
 Fixed	Fixed::operator -- (int)
 {
-	Fixed	old(*this);
+	Fixed	original(*this);
 
-	--(*this);
-	return (old);
+	(*this).fixed_point_value -= 1;
+	return (original);
 }
 
 /*	min . max		*/
