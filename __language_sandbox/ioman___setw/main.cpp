7252d8c7f6d148627cf8958b9adef38721ad3713
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-10 20:42:41 +0100

    edit setw

--- __language_sandbox/ioman___setw/main.cpp
+++ __language_sandbox/ioman___setw/main.cpp
@@ -2,12 +2,31 @@
 #include "sstream"
 #include "iomanip"
 
+#define	W0 10
+#define	W1 6
+#define	W2 9
+
 int	main()
 {
-	std::cout << "no setw: [" << 42 << "]\n"
-	<< "setw(6): [" << std::setw(6) << "]\n"
-	<< "setw(6): [" << std::setw(6) << 42 << "]\n"
-	<< "setw(6): [" << 89 << std::setw(6) << 12 << 34 << "]\n"
-	<< "setw(6): [" << 89 << 64 << std::setw(6) << 12 << 34 << "]"
-	<< std::endl;
+	std::cout << "no setw: " << "\n"
+	<< "[" << 42 << "]" << "\n\n"
+
+	<< W0 << "\n"
+	<< "[" << std::setw(W0) << "]\n"
+	<< "[" << std::setw(W0) << 42 << "]\n"
+	<< "[" << 89 << std::setw(W0) << 12 << 34 << "]\n"
+	<< "[" << 89 << 64 << std::setw(W0) << 12 << 34 << "]"
+	<< "\n\n"
+	
+	<< W1 << '\n'
+	<< "[" << std::setw(W1) << "]\n"
+	<< "[" << std::setw(W1) << 42 << "]\n"
+	<< "[" << 89 << std::setw(W1) << 12 << 34 << "]\n"
+	<< "[" << 89 << 64 << std::setw(W1) << 12 << 34 << "]"
+	<< "\n\n"
+
+	<< W2 << '\n'
+	<< "[" << std::setfill('.') << 42 << std::setw(W2) << "]\n"
+	<< "[" << std::setfill('.') << 1 << 2 << 3 << 4 << "Five" << 99 << std::setw(W2)
+	<< "]";
 }
