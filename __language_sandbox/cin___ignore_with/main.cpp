a3ddb7b91047d95ec206c37af0c1e7678c9ad55a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-10 08:56:51 +0100

    added ignore box with good sample

--- __language_sandbox/cin___ignore_with/main.cpp
+++ __language_sandbox/cin___ignore_with/main.cpp
@@ -1,5 +1,6 @@
 #include "iostream"
 #include "string"
+#include "limits"
 
 int	main()
 {
@@ -7,16 +8,20 @@ int	main()
 	std::string		s;
 
 	//	Prompt user for input
+
 	std::cout << "enter an int & a string: ";
 
 	//	Get input
+
 	std::cin >> x;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::getline(std::cin, s);
 
 	//	Show input
+
 	std::cout << "your integer:\t" << x << '\n';
 	std::cout << "your string:\t" << s << '\n';
 
 	//	Explanation
-	std::cout << "\n(*) it works strangely without cin.ignore \n";
+	std::cout << "\n(*) now with cin.ignore, it works beautifully \n";
 }
