664fab9ea12a57e74376868bba4cb404a927f0f3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-04 18:40:32 +0100

    update zombie

--- cpp1_submit/ex00/main.cpp
+++ cpp1_submit/ex00/main.cpp
@@ -14,12 +14,12 @@
 /* *********************  ʕ • ᴥ•ʔ  mode: todo  (⊙. ⊙ )  ********************* */
 
 #include "Zombie.hpp"
-
+/*
 static void	storyline_1(void);
 static void	storyline_2(void);
 static void	storyline_3(void);
 static void	storyline_4(void);
-
+*/
 int	main()
 {
 	Zombie	matt("Matthew");
@@ -50,54 +50,3 @@ int	main()
 
 //
 
-static void	storyline_1(void)
-{
-	std::cout << "\nStoryline (1)";
-	std::cout << _YELLOW_ "\n[ Meet Matthew, the first ";
-	std::cout << "man-made psychic-vampire of ";
-	std::cout << "the new era. ]\n" _REST_ ;
-	
-	std::cout << _CYAN_ "[ Matthew is created on the ";
-	std::cout << _RED_ "\"Stack\"" _REST_ ;
-	
-	std::cout << _CYAN_ " ]\n[ ie. built with ";
-	std::cout << _RED_ _ITAL_"\"Zombie name(name)\"" _REST_ ;
-	
-	std::cout << _CYAN_ " ]\n\n" _REST_ ;
-
-}
-
-static void	storyline_2(void)
-{
-	std::cout << "\nStoryline (2)";
-	
-	std::cout << _YELLOW_ "\n[ Here's Luke. Luke is different... ]\n";
-	std::cout << "[ While Matthew is a vampire, Luke is a ghost. ";
-	std::cout << "...A real one. ]\n" _REST_ ;
-	
-	std::cout << _CYAN_ "[ Luke is created on the ";
-	std::cout << _RED_ "\"Heap\"" _REST_ ;
-	
-	std::cout << _CYAN_ " ]\n[ ie. built by using the ";
-	std::cout << _RED_ _ITAL_ "\"new\"" _REST_ ;
-	
-	std::cout << _CYAN_ " keyword ]\n\n" _REST_ ;
-}
-
-static void	storyline_3(void)
-{
-	std::cout << "\nStoryline (3)";
-	std::cout << _YELLOW_ "\n[ Paul is born... ";
-	std::cout << "and dies instantly. ]\n\n" _REST_;
-}
-
-static void	storyline_4(void)
-{
-	std::cout << "\nStoryline (4)";
-	
-	std::cout << _YELLOW_ "\n[ Let's kill Luke before we go. ]\n";
-	std::cout << "[ Because he is the ";
-	std::cout << _ITAL_ _RED_ "heap " _REST_ << _YELLOW_ "guy. ]\n";
-	
-	std::cout << "[ We didn't kill Matthew. ]\n\n" _REST_;
-}
