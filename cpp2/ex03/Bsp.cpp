758394025782bc83102ef1f5166e91bccc8ac048
nuoxoxo <nuxu@e1r1p1.clusters.42paris.fr>
2023-01-09 14:10:22 +0100

    fi. bsp

--- cpp2/ex03/Bsp.cpp
+++ cpp2/ex03/Bsp.cpp
@@ -32,37 +32,48 @@ static float    get_sign(Point, Point, Point);
 
 bool bsp( Point const a, Point const b, Point const c, Point const to_check)
 {
+    bool    has_same_sign;
     float   s1, s2, s3;
-    bool    is_negative, is_positive;
     
-    s1 = get_sign(to_check, b, a);
-    s2 = get_sign(to_check, a, c);
-    s3 = get_sign(to_check, c, b);
+    s1 = get_sign(to_check, a, b);
+    s2 = get_sign(to_check, b, c);
+    s3 = get_sign(to_check, c, a);
 
-    is_negative = s1 < 0 || s2 < 0 || s3 < 0;
-    is_positive = s1 > 0 || s2 > 0 || s3 > 0;
+    if (!s1 || !s2 || !s3)
+        return false;
 
-    return (is_negative || is_positive);
+    has_same_sign = (
+        (s1 < 0 || s2 < 0 || s3 < 0) ||
+        (s1 > 0 || s2 > 0 || s3 > 0)
+    );
+
+    return (has_same_sign);
 }
 
 
 static float    get_sign(Point p1, Point p2, Point Ref)
 {
-    return
-    (
-        // (x1 - x) * 
-        // (y2 - y)
-        //  minus
-        // (x2 - x) * 
-        // (y1 - y)
-        
-        (p1.getX().toFloat() - Ref.getX().toFloat())
-        *
+    //  Formula
+    //      (x1 - x) * 
+    //      (y2 - y)
+    //          subtract
+    //      (x2 - x) * 
+    //      (y1 - y)
+
+    float res = 
+        (p1.getX().toFloat() - Ref.getX().toFloat()) *
         (p2.getY().toFloat() - Ref.getY().toFloat())
-         - 
-        (p2.getX().toFloat() - Ref.getX().toFloat())
-        *
+        -
+        (p2.getX().toFloat() - Ref.getX().toFloat()) *
         (p1.getY().toFloat() - Ref.getY().toFloat())
-    );
+    ;
+
+    //  Debugger
+    //      std::cout << p1.getX() << ' ' << p1.getY() << std::endl;
+    //      std::cout << p2.getX() << ' ' << p2.getY() << std::endl;
+    //      std::cout << Ref.getX() << ' ' << Ref.getY() << std::endl;
+    //      std::cout << res << std::endl;
+
+    return (res);
 }
 
--- cpp2/ex03/main.cpp
+++ cpp2/ex03/main.cpp
@@ -15,37 +15,86 @@
 
 int main()
 {
-    Point const p1(0, 0);
-    Point const p2(30, 0);
-    Point const	p3(15, 15);
-	
-	bool		flag;
-    
-    
-	std::cout << "p1 " << "(" << p1.getX().toFloat() << "," << p1.getY().toFloat() << ")\n";
-    std::cout << "p2 " << "(" << p2.getX().toFloat() << "," << p2.getY().toFloat() << ")\n";
-    std::cout << "p3 " << "(" << p3.getX().toFloat() << "," << p3.getY().toFloat() << ")\n";
-    
 	// Test 1
 	{
-		Point const o1(12, 2);
-		std::cout << "o1 " << "(" << o1.getX().toFloat() << "," << o1.getY().toFloat() << ")\n";
+		Point const p1(0, 0);
+		Point const p2(10, 30);
+		Point const	p3(20, 0);
+
+		//           b {10,30}
+		//             / \
+		//            /   \
+		//           /     \
+		//          /   . P {10, 15}
+		//         /         \
+		//  a {0,0} ---------- c {20,0} 
+
+		std::cout << CYAN "Point 1 \ta " << REST "{" << p1.getX().toFloat() << ", " << p1.getY().toFloat() << "}\n";
+		std::cout << CYAN "Point 2 \tb " << REST "{" << p2.getX().toFloat() << ", " << p2.getY().toFloat() << "}\n";
+		std::cout << CYAN "Point 3 \tc " << REST "{" << p3.getX().toFloat() << ", " << p3.getY().toFloat() << "}\n";
+
+		Point const	P(10, 15);
+		std::cout << YELL "Inspect \tP " << REST "{" << P.getX().toFloat() << ", " << P.getY().toFloat() << "}\n";
 		
-		flag = bsp(p1,p2,p3,o1);
-		if(flag) 
-			std::cout << "Yes\n";
-		else 
-			std::cout << "No\n";
+		if (bsp(p1, p2, p3, P))
+			std::cout << "Result  \t" GREEN "Yes it is." REST;
+		else
+			std::cout << "Result  \t" RED "No it is not." REST;
+		std::cout << nl2;
 	}
 	// Test 2
 	{
-		Point const o2(12, 2);
-		std::cout << "o2 " << "(" << o2.getX().toFloat() << "," << o2.getY().toFloat() << ")\n";
+		Point const p1(0, 0);
+		Point const p2(10, 30);
+		Point const	p3(20, 0);
+
+		//				.	b, `P` {10,30}
+		//             / \
+		//            /   \
+		//           /     \
+		//          /       \
+		//         /         \
+		//  a {0,0} ---------- c {20,0}
+
+		std::cout << CYAN "Point 1 \ta " << REST "{" << p1.getX().toFloat() << ", " << p1.getY().toFloat() << "}\n";
+		std::cout << CYAN "Point 2 \tb " << REST "{" << p2.getX().toFloat() << ", " << p2.getY().toFloat() << "}\n";
+		std::cout << CYAN "Point 3 \tc " << REST "{" << p3.getX().toFloat() << ", " << p3.getY().toFloat() << "}\n";
+
+		Point const	P(10, 30);
+		std::cout << YELL "Inspect \tP " << REST "{" << P.getX().toFloat() << ", " << P.getY().toFloat() << "}\n";
 		
-		flag = bsp(p1,p2,p3,o2);
-		if(flag) 
-			std::cout << "Yes\n";
-		else 
-			std::cout << "No\n";
-	};
+		if (bsp(p1, p2, p3, P))
+			std::cout << "Result  \t" GREEN "Yes it is." REST;
+		else
+			std::cout << "Result  \t" RED "No it is not." REST;
+		std::cout << nl2;
+	}
+	// Test 3
+	{
+		Point const p1(0, 0);
+		Point const p2(10, 30);
+		Point const	p3(20, 0);
+
+		//           	b
+		//				.
+		//             / \
+		//            /   \
+		//           /     \
+		//          /       \
+		//         /         \
+		//  a {0,0} ---------- c {20,0} 
+
+		std::cout << CYAN "Point 1 \ta " << REST "{" << p1.getX().toFloat() << ", " << p1.getY().toFloat() << "}\n";
+		std::cout << CYAN "Point 2 \tb " << REST "{" << p2.getX().toFloat() << ", " << p2.getY().toFloat() << "}\n";
+		std::cout << CYAN "Point 3 \tc " << REST "{" << p3.getX().toFloat() << ", " << p3.getY().toFloat() << "}\n";
+
+		Point const	P(1000, 3000);
+		std::cout << YELL "Inspect \tP " << REST "{" << P.getX().toFloat() << ", " << P.getY().toFloat() << "}\n";
+		
+		if (bsp(p1, p2, p3, P))
+			std::cout << "Result  \t" GREEN "Yes it is." REST;
+		else
+			std::cout << "Result  \t" RED "No it is not." REST;
+		std::cout << nl2;
+	}
 }
