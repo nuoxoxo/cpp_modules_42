81ed2ea2925d91ea973a4d30626df74da032974a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-13 19:38:42 +0100

    on brain c/hpp

--- cpp4/ex01/Brain.cpp
+++ cpp4/ex01/Brain.cpp
@@ -10,30 +10,66 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BRAIN_HPP
-# define BRAIN_HPP
+#include "Cat.hpp"
 
-# include "iostream"
-# include "string"
 
-# define N 100
+// constructor . destructor
 
-class Brain
+Cat::Cat() : Animal("Cat")
 {
+	std::cout << LOWKEY << __FUNCTION__
+	<< " Constructor" << called REST;
+}
 
-private:
-	std::string	m_idea[N];
-
-public:
-	// canon
-	Brain();
-	~Brain();
-	Brain(const Brain &);
-	Brain & operator = (const Brain &);
-
-	// what to do with the idea[100] (?)
-};
+Cat::~Cat()
+{
+	std::cout << LOWKEY << __FUNCTION__
+	<< " Destructor" << called REST;
+}
 
 
-#endif
+// copy
+
+Cat::Cat(const Cat & dummy)
+{
+	*this = dummy;
+
+	std::cout << LOWKEY << __FUNCTION__
+	<< " Copy constructor" << called REST;
+
+}
+
+
+// copy assignement = 
+
+Cat & Cat::operator = (const Cat & dummy)
+{
+	this->m_type = dummy.m_type;
+	std::cout << LOWKEY << __FUNCTION__
+	<< " Copy assignment constructor" << called REST;
+
+	return (*this);
+
+}
+
+
+// .method()
+
+void	Cat::makeSound() const
+{
+	std::string	sound_of_cats[3] = \
+	{
+		"\"Mew ~~ \"",
+		"\"Meow ~~ \"", 
+		"\"Purr ~~ \""
+	};
+
+	srand(time(0));
+	int n = (int) rand() % 3;
+	// srand() debugger
+	// std::cout << r << ' ' << n << std::endl;
+
+	std::cout << m_type << ": " YELL << sound_of_cats[n] << REST nl;
+}
+
 
