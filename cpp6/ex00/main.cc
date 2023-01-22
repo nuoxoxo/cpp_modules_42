81bf44160b26e9c14c3369c69a08ffe8467d14b4
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-22 21:37:56 +0100

    adding methods .

--- cpp6/ex00/main.cc
+++ cpp6/ex00/main.cc
@@ -44,22 +44,24 @@ public:
 // Canon
 Convertor::Convertor() {
 	print_canon(std::string(__FUNCTION__), "Constructor");
-};
+}
 
 Convertor::~Convertor() {
 	print_canon(std::string(__FUNCTION__), "Destructor");
-};
+}
 
 Convertor::Convertor(Convertor const & dummy) {
 	print_canon(std::string(__FUNCTION__), "Copy Constructor");
 	*this = dummy;
-};
+}
 
 Convertor & Convertor::operator = (convertor const & dummy) {
-	print_canon(std::string(__FUNCTION__), "Destructor");
+	print_canon(std::string(__FUNCTION__), "Copy assignment");
 	if (this != & dummy)
 		this->m_core_string = dummy.m_core_string;
 	return (*this);
-};
+}
 
-
+Convertor::Convertor(std::string s) : m_core_string(s) {
+	print_canon(std::string(__FUNCTION__), "Param constructor");
+}
