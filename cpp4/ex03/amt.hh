ae66271f0fbbf060e6c76a34e0ca9dd973f8a4d0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-22 14:10:07 +0100

    drafting . amt x4

---
+++ cpp4/ex03/amt.hh
@@ -0,0 +1,20 @@
+#include "ICR.hh"
+
+class	icr;
+
+class	amt
+{
+
+std::string	m_type;
+public:
+	amt();
+	virtual ~amt();
+	amt(amt const &);
+	amt(std::string const &);
+	amt & operator = (amt const &);
+
+	std::string const & getType() const;
+
+	virtual amt * clone() const = 0;
+	virtual void use(irc &);
+};
