a6adcfa5b90257c7165c86acb6aa8d5c57f3e320
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-24 10:40:09 +0100

    added substr sandbox

--- cpp6/ex00/main.cpp
+++ cpp6/ex00/main.cpp
@@ -4,22 +4,31 @@
 #include "iomanip"
 #include "limits"
 
-#include "cstring"
+/*struct	_Scalar_
+{
+	char	c;
+	int	i;
+	float	f;
+};*/
 
 static void	_usage_();
+static bool	strIsInt(const std::string &);
 static bool	strIsChar(const std::string &);
-
-
-
+//static bool	strIsFloat(const std::string &);
+//static bool	strIsDouble(const std::string &);
 
 int	main(int c, char **v)
 {
 	if (c ^ 2)
 		return (_usage_(), 1);
 
+	std::cout << strIsInt(std::string(v[1])) << std::endl;
+	std::cout << strIsChar(std::string(v[1])) << std::endl;
+
 	(void) c, (void) v;
 	return 0;
 }
+
 
 //
 
@@ -28,9 +37,48 @@ static void	_usage_()
 	std::cout << nl YELL "Usage: ./convert _literal_ " RESET nl2;
 }
 
-static bool	strIsChar(const std::string & s)
-{
-	return (s.length() == 3 && s[0] == '\'' && s[0] == s[2]);
-}
+
+//	char . num (int) . float . double
 
 static bool	strIsChar(const std::string & s)
+{
+	return (s.length() == 3 && s[0] == '\'' && s[2] == '\'');
+}
+
+static bool	strIsInt(const std::string & s)
+{
+	if (s.empty())
+		return false;
+
+	std::string::const_iterator	it = s.begin();
+
+	while (it != s.end() && std::isdigit(*it))
+		++it;;
+	return (it == s.end());
+}
+
+static bool	strIsFloat(const std::string & s)
+{
+	if (isMacroFloat(s))
+		return true;
+
+	std::string	t;
+	int		len = (int) s.length();
+
+	if (s[len - 1] != 'f')
+		return false;
+	t = s.substr(0, len - 1);
+}
+
+
+//	is macro group
+
+bool	isMacroFloat(const std::string & s)
+{
+	return (s == "inff" || s == "-inff" || s == "+inff" || s == "nanf");
+}
+
+bool	isMacroDouble(const std::string & s)
+{
+	return (s == "inf" || s == "-inf" || s == "+inf" || s == "nan");
+}
