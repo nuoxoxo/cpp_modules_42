e0998bb4d99be88e0ae4fd936810590176eae95b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-24 18:16:41 +0100

    2ways

--- cpp6/ex02/Cpp06x2.hpp
+++ cpp6/ex02/Cpp06x2.hpp
@@ -21,7 +21,7 @@ Base	*generate(void)
 
 void	identify(Base* na)
 {
-	std::cout << __FUNCTION__ << "(Base *): ";
+	std::cout << __FUNCTION__ << " (Base *): ";
 	if (dynamic_cast<A *>(na)) {
 
 		std::cout << "A \n";
@@ -45,12 +45,14 @@ void	identify(Base& p)
 {
 	Base	dummy;
 
-	std::cout << __FUNCTION__ << "(Base &): ";
-
+	std::cout << __FUNCTION__ << " (Base &): ";
+ 
 	try {
 
-		dummy = dynamic_cast<A&>(p);
+		A	& a = dynamic_cast<A &>(p); // 1st way
+
 		std::cout << "A" << std::endl;
+		(void) a;
 		return ;
 
 	} catch (const std::exception & e) {
@@ -59,7 +61,7 @@ void	identify(Base& p)
 	
 	try {
 
-		dummy = dynamic_cast<B&>(p);
+		dummy = dynamic_cast<B &>(p); // 2nd way
 		std::cout << "B" << std::endl;
 		return ;
 
@@ -69,7 +71,7 @@ void	identify(Base& p)
 	
 	try {
 
-		dummy = dynamic_cast<C&>(p);
+		dummy = dynamic_cast<C &>(p);
 		std::cout << "C" << std::endl;
 		return ;
 
