ee1c8177082153a7267bd5dfd8409b40d89b45ce
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-25 18:15:38 +0100

    draft

---
+++ _drafts_/draft_p07_pre_submit/ex01/iter.hpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                             ~  ~  ~  ~  ~    ~                             */
+/*                              ~    _ ~ _   o>                               */
+/*                             ~  \ / \ / \ /  ~                              */
+/*                              ~  ~      ~    ~                              */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                 Template                                   */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/* *********************  ʕ • ᴥ•ʔ  mode: todo  (⊙. ⊙ )  ********************* */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include "iostream"
+# include "string"
+
+# define LOWKEY	"\033[0;2m"
+# define GREEN	"\033[0;32m"
+# define RESET	"\033[0;0m"
+
+template<typename T>
+void	iter(T * arr, size_t len, void (*f)(T &))
+{
+	size_t	i = -1;
+
+	while (++i < len)
+		f(arr[i]);
+}
+
+template <typename T>
+void	print(T & elem)
+{
+	std::cout << elem << LOWKEY " (" << __FUNCTION__ << ") \n" RESET;
+}
+
+template <typename T>
+void	left_shift_1(T & elem)
+{
+	std::cout << LOWKEY "(" << __FUNCTION__ << ") \n" RESET;
+
+	try
+	{
+		elem <<= 1;
+	}
+	catch (std::exception(& e))
+	{
+		;;
+	}
+}
+
+template <typename T>
+void	right_shift_2(T & elem)
+{
+	std::cout << LOWKEY "(" << __FUNCTION__ << ") \n" RESET;
+
+	try
+	{
+		elem >>= 2;
+	}
+	catch (std::exception(& e))
+	{
+		;;
+	}
+}
+
+#endif
---
+++ _drafts_/draft_p07_pre_submit/ex02/main.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                             ~  ~  ~  ~  ~    ~                             */
+/*                              ~    _ ~ _   o>                               */
+/*                             ~  \ / \ / \ /  ~                              */
+/*                              ~  ~      ~    ~                              */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                   Array                                    */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/* *********************  ʕ • ᴥ•ʔ  mode: todo  (⊙. ⊙ )  ********************* */
+
+#include "Array.hpp"
+#define MAX 4294967295
+
+#define LOWKEY	"\033[0;2m"
+#define GREEN	"\033[0;32m"
+#define RESET	"\033[0;0m"
+
+int	main()
+{
+	int	i = -1;
+
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+		Array<int>	arr(2);
+
+		arr.printer();
+		std::cout << arr << std::endl;
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+}
+
