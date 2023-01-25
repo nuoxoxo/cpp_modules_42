cc1f5989efa44039b84d28f9244d039bc352b342
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-25 10:33:32 +0100

    init

---
+++ cpp7/ex02/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                             ~  ~  ~  ~  ~    ~                             */
+/*                              ~    _ ~ _   o>                               */
+/*                             ~  \ / \ / \ /  ~                              */
+/*                              ~  ~      ~    ~                              */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                 Template                                   */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/*                                                                            */
+/* *********************  ʕ • ᴥ•ʔ  mode: todo  (⊙. ⊙ )  ********************* */
+
+#include "iter.hpp" // TODO
+#include "iomanip"
+
+int	main()
+{
+	int	i = -1;
+
+	std::cout << GREEN "\nTest :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "\nTest :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+	std::cout << GREEN "Test :: " << ++i << "\n\n" RESET;
+	{
+	}
+}
+
