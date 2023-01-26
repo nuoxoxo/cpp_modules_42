3769bbd610031f90974653249d0a3d2b7a41304c
nuoxoxo <nuxu@e2r4p14.clusters.42paris.fr>
2023-01-26 20:08:17 +0100

    iMatSce

--- cpp4/ex03/IMateriaSource.hpp
+++ cpp4/ex03/IMateriaSource.hpp
@@ -16,26 +16,20 @@
 /*                                                                            */
 /* *********************  ʕ • ᴥ•ʔ  mode: todo  (⊙. ⊙ )  ********************* */
 
-#ifndef ICHARACTER_HPP
-# define ICHARACTER_HPP
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
 
-# include "iostream"
 # include "AMateria.hpp"
 
-class	AMateria;
-
-class	ICharacter
+class	IMateriaSource
 {
 
 public:
 
-	virtual	~ICharacter() {}
+	virtual	~IMateriaSource() {}
 
-	virtual		std::string const & getName() const = 0;
-
-	virtual	void	use(int, ICharacter & )	= 0;
-	virtual void	equip(AMateria * ) = 0;
-	virtual void	unequip(int) = 0;
+	virtual void		learnMateria( AMateria * ) = 0;
+	virtual AMateria*	createMateria(std::string const &) = 0;
 };
 
 #endif
