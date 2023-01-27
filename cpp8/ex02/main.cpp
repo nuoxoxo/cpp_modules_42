9a1bc5bb028e34956005e0c621aea28b596ae8e8
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-27 19:55:45 +0100

    absolutely no idea how to do this one

--- cpp8/ex02/main.cpp
+++ cpp8/ex02/main.cpp
@@ -1,8 +1,9 @@
 
 
-int main()
+int	main()
 {
-	MutantStack<int> mstack;
+	MutantStack<int>	mstack;
+
 	mstack.push(5);
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
@@ -11,17 +12,24 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
+
 	//[...]
+
 	mstack.push(0);
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+
 	++it;
 	--it;
+
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
-	std::stack<int> s(mstack);
+
+	std::stack<int>	s(mstack);
+
 	return 0;
 }
