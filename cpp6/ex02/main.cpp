e1ec8191fcca94f20e373434227ca7d6fe246db0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-27 11:31:33 +0100

    rename header

--- cpp6/ex02/main.cpp
+++ cpp6/ex02/main.cpp
@@ -18,7 +18,7 @@
 /*                                                                            */
 /* *********************  ʕ • ᴥ•ʔ  mode: todo  (⊙. ⊙ )  ********************* */
 
-#include "Cpp06x2.hpp"
+#include "CPP06X2.hpp"
 #include "Base.hpp"
 #include "A.hpp"
 #include "B.hpp"
