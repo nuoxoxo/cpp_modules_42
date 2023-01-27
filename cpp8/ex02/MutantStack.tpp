10a6985846f624505dfd97a869726700db7aba4d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-27 23:36:31 +0100

    tpp

---
+++ cpp8/ex02/MutantStack.tpp
@@ -0,0 +1,65 @@
+#ifndef MUTANTSTACK_TPP
+# define MUTANTSTACK_TPP
+
+# include "MutantStack.hpp"
+
+template <typename T>
+MutantStack<T>::MutantStack() : std::stack<T> () {}
+
+template <typename T>
+MutantStack<T>::~MutantStack<T> () {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack & dummy)
+{
+	*this = dummy;
+}
+
+template <typename T>
+MutantStack<T> & MutantStack<T>::operator = (const MutantStack<T> & dummy)
+{
+	this->c = dummy.c;
+	return (*this);
+}
+
+template <typename T>
+unsigned int	MutantStack<T>::size() const
+{
+	return (this->c.size());
+}
+
+template <typename T>
+const T & MutantStack<T>::top() const
+{
+	return (this->c.back());
+}
+
+template <typename T>
+bool	MutantStack<T>::empty() const
+{
+	return (this->c.empty());
+}
+
+template <typename T>
+void	MutantStack<T>::push(T item) {
+	this->c.push_back(item);
+}
+
+template <typename T>
+void	MutantStack<T>::pop() {
+	this->c.pop_back(); // undefined if empty
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return (iterator(this->c.begin()));
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return (iterator(this->c.end()));
+}
+
+#endif
--- cpp8/ex02/Mutantstack.hpp
+++ cpp8/ex02/Mutantstack.hpp
@@ -1,5 +1,5 @@
 #ifndef MUTANTSTACK_HPP
-#define MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
 # include "iostream"
 # include "deque"
@@ -19,7 +19,7 @@ public:
 	~MutantStack();
 
 	// methods
-	int		size() const;
+	unsigned int	size() const;
 
 	void		push(T);
 	void		pop();
@@ -33,70 +33,6 @@ public:
 	typename std::deque<T>::iterator	end();
 };
 
-template <typename T>
-MutantStack<T>::MutantStack() : std::stack<T> () {}
-
-template <typename T>
-MutantStack<T>::~MutantStack<T> () {}
-
-template <typename T>
-MutantStack<T>::MutantStack(const MutantStack & dummy)
-{
-	*this = dummy;
-}
-
-template <typename T>
-MutantStack<T> & MutantStack<T>::operator = (const MutantStack<T> & dummy)
-{
-	this->c = dummy.c;
-	return (*this);
-}
-
-
-
-template <typename T>
-const T & MutantStack<T>::top() const
-{
-	return (this->c.back());
-}
-
-template <typename T>
-bool	MutantStack<T>::empty() const
-{
-	return (this->c.empty());
-}
-
-template <typename T>
-void	MutantStack<T>::push(T item) {
-	this->c.push_back(item);
-}
-
-template <typename T>
-void	MutantStack<T>::pop() {
-	this->c.pop_back(); // undefined if empty
-}
-
-template <typename T>
-typename MutantStack<T>::iterator MutantStack<T>::begin()
-{
-	return (typename std::deque<T>::iterator(this->c.begin()));
-}
-
-template <typename T>
-typename MutantStack<T>::iterator MutantStack<T>::end()
-{
-	return (typename std::deque<T>::iterator(this->c.end()));
-}
-
-/*Span::Span() : m_maxsize() {}
-Span::Span(unsigned int N) : m_maxsize(N) {}
-Span::Span(const Span &cp) : m_core(cp.getCore()), m_maxsize(cp.getMaxsize()){}
-Span::~Span() {}*/
-
-/*
-template<typename T>
-typename T::iterator	easyfind(T arr, int val)
-*/
-
+# include "MutantStack.tpp"
 
 #endif
